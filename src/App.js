4b114d40445bba52782756488e1b90209aec5907
muhammad kashiff <muhammad.kashif@ceative.co.uk>
2023-04-05 16:55:09 +0500

    jobs

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import {
   Link
 } from "react-router-dom";
 import Signup from './components/Pages/AuthPages/Signup';
+import JobCategories from './components/Pages/Jobs/JobCategories/JobCategories';
 
 function App() {
   return (
     <Routes>
       <Route exact path={"/login"} element={<Login />} />
       <Route exact path='/signup' element={<Signup />} />
+      <Route exact path='/job-categories' element={<JobCategories />} />
     </Routes>
   );
 }
---
+++ src/components/Pages/Jobs/JobCategories/JobCategories.jsx
@@ -0,0 +1,18 @@
+import JobHome from "./jobHome/JobHome"
+import JobCard from "./JobCard/JobCard"
+
+// import JobSearchBanner from "./JobSearchBanner/JobSearchBanner"
+
+const JobCategories=()=>{
+    
+    return (
+        <>
+        <JobHome/>
+        <JobCard/>
+        {/* <JobSearchBanner/> */}
+       
+        </>
+    )
+
+}
+export default JobCategories
