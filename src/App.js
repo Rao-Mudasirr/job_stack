cb8488afdd2d419a782d27fe89ec746f4f200d48
AliHassanS37 <ali.hassan91172@gmail.com>
2023-04-05 11:35:53 +0500

    Added about-us page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
-import Login from './components/Pages/AuthPages/Login';
 import {
   Routes,
   Route,
   Link
 } from "react-router-dom";
+import Login from './components/Pages/AuthPages/Login';
 import Signup from './components/Pages/AuthPages/Signup';
+import AboutUs from './components/Pages/AboutUs/AboutUs';
 
 function App() {
   return (
     <Routes>
       <Route exact path={"/login"} element={<Login />} />
       <Route exact path='/signup' element={<Signup />} />
+      <Route exact path='/about-us' element={<AboutUs />} />
     </Routes>
   );
 }
