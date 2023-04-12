5ecb62062481108ee5a5819276a6045ffcb486ff
shahbaz mughal <shahbaz.mughal@ceative.co.uk>
2023-04-12 14:54:54 +0500

    shahbaz

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import './App.css';
-import Login from './components/Pages/AuthPages/Login';
-import {
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import Signup from './components/Pages/AuthPages/Signup';
-import TermsAndServices from './components/Pages/TermsAndServices/TermsAndServices.tsx';
-import PrivacyPolicy from './components/Pages/PrivacyPolicy/PrivacyPolicy.tsx';
-import Error404 from './components/Pages/404/404.tsx';
-import CommingSoon from './components/Pages/CommingSoon/CommingSoon.tsx';
-import Maintenance from './components/Pages/Maintenance/Maintenance.tsx';
-import ThankYou from './components/Pages/ThankYou/ThankYou.tsx';
+import "./App.css";
+import Login from "./components/Pages/AuthPages/Login";
+import { Routes, Route, Link } from "react-router-dom";
+import Signup from "./components/Pages/AuthPages/Signup";
+import TermsAndServices from "./components/Pages/TermsAndServices/TermsAndServices.tsx";
+import PrivacyPolicy from "./components/Pages/PrivacyPolicy/PrivacyPolicy.tsx";
+import Error404 from "./components/Pages/404/404.tsx";
+import CommingSoon from "./components/Pages/CommingSoon/CommingSoon.tsx";
+import Maintenance from "./components/Pages/Maintenance/Maintenance.tsx";
+import ThankYou from "./components/Pages/ThankYou/ThankYou.tsx";
+import Layout from "./components/Pages/Layout/Layout.tsx";
 
 function App() {
   return (
     <Routes>
       <Route exact path={"/login"} element={<Login />} />
-      <Route exact path='/signup' element={<Signup />} />
-      <Route exact path='/terms' element={<TermsAndServices />} />
-      <Route exact path='/privacy' element={<PrivacyPolicy />} />
-      <Route exact path='/comingsoon' element={<CommingSoon />} />
-      <Route exact path='/maintenance' element={<Maintenance />} />
-      <Route exact path='/error' element={<Error404 />} />
-      <Route exact path='/thankyou' element={<ThankYou />} />
+      <Route exact path="/signup" element={<Signup />} />
 
+      <Route
+        exact
+        path="/terms"
+        element={
+          <Layout>
+            <TermsAndServices />
+          </Layout>
+        }
+      />
+      <Route
+        exact
+        path="/privacy"
+        element={
+          <Layout>
+            <PrivacyPolicy />
+          </Layout>
+        }
+      />
+      <Route exact path="/comingsoon" element={<CommingSoon />} />
+      <Route exact path="/maintenance" element={<Maintenance />} />
+      <Route exact path="/error" element={<Error404 />} />
+      <Route exact path="/thankyou" element={<ThankYou />} />
     </Routes>
   );
 }
--- src/components/Pages/PrivacyPolicy/PrivacyPolicy.tsx
+++ src/components/Pages/PrivacyPolicy/PrivacyPolicy.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 const PrivacyPolicy = () => {
   return (
     <div>
-       {/* <nav id="topnav" className="defaultscroll is-sticky">
+      {/* <nav id="topnav" className="defaultscroll is-sticky">
             <div className="container">
                 
                 <a className="logo" href="index.html">
@@ -166,61 +166,128 @@ const PrivacyPolicy = () => {
                 </div>
             </div>
         </nav> */}
-        <section className="relative table w-full py-36 ">
-            <div className="absolute inset-0 bg-emerald-900/90"></div>
-            <div className="container">
-                <div className="grid grid-cols-1 text-center mt-10">
-                    <h3 className="md:text-3xl text-2xl md:leading-snug tracking-wide leading-snug font-medium text-white">Privacy Policy</h3>
-                </div>
-            </div>
-            
-            <div className="absolute text-center z-10 bottom-5 right-0 left-0 mx-3">
-                <ul className="breadcrumb tracking-[0.5px] breadcrumb-light mb-0 inline-block">
-                   <li className="inline breadcrumb-item text-[15px] font-semibold duration-500 ease-in-out text-white/50 hover:text-white"><a href="index.html">Jobstack</a></li>
-                    <li className="inline breadcrumb-item text-[15px] font-semibold duration-500 ease-in-out text-white" aria-current="page">Privacy Policy</li>
-                </ul>
-            </div>
-        </section>
-        <div className="relative">
-            <div className="shape overflow-hidden z-1 text-white dark:text-slate-900">
-                <svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z" fill="currentColor"></path>
-                </svg>
-            </div>
+      <section className="relative table w-full py-36 ">
+        <div className="absolute inset-0 bg-emerald-900/90"></div>
+        <div className="container">
+          <div className="grid grid-cols-1 text-center mt-10">
+            <h3 className="md:text-3xl text-2xl md:leading-snug tracking-wide leading-snug font-medium text-white">
+              Privacy Policy
+            </h3>
+          </div>
         </div>
-        
-        <section className="relative lg:py-24 py-16">
-            <div className="container">
-                <div className="md:flex justify-center">
-                    <div className="md:w-3/4">
-                        <div className="p-6 bg-white dark:bg-slate-900 shadow dark:shadow-gray-700 rounded-md">
-                            <h5 className="text-xl font-medium mb-4">Overview :</h5>
-                            <p className="text-slate-400">It seems that only fragments of the original text remain in the Lorem Ipsum texts used today. One may speculate that over the course of time certain letters were added or deleted at various positions within the text.</p>
-                            <p className="text-slate-400">In the 1960s, the text suddenly became known beyond the professional circle of typesetters and layout designers when it was used for Letraset sheets (adhesive letters on transparent film, popular until the 1980s) Versions of the text were subsequently included in DTP programmes such as PageMaker etc.</p>
-                            <p className="text-slate-400">There is now an abundance of readable dummy texts. These are usually used when a text is required purely to fill a space. These alternatives to the classic Lorem Ipsum texts are often amusing and tell short, funny or nonsensical stories.</p>
-                        
-                            <h5 className="text-xl font-medium mb-4 mt-8">We use your information to :</h5>
-                            <ul className="list-unstyled text-slate-400 mt-4">
-                                <li className="flex mt-2"><i className="uil uil-arrow-right text-emerald-600 align-middle ltr:mr-2 rtl:ml-2"></i>Digital Marketing Solutions for Tomorrow</li>
-                                <li className="flex mt-2"><i className="uil uil-arrow-right text-emerald-600 align-middle ltr:mr-2 rtl:ml-2"></i>Our Talented & Experienced Marketing Agency</li>
-                                <li className="flex mt-2"><i className="uil uil-arrow-right text-emerald-600 align-middle ltr:mr-2 rtl:ml-2"></i>Create your own skin to match your brand</li>
-                                <li className="flex mt-2"><i className="uil uil-arrow-right text-emerald-600 align-middle ltr:mr-2 rtl:ml-2"></i>Digital Marketing Solutions for Tomorrow</li>
-                                <li className="flex mt-2"><i className="uil uil-arrow-right text-emerald-600 align-middle ltr:mr-2 rtl:ml-2"></i>Our Talented & Experienced Marketing Agency</li>
-                                <li className="flex mt-2"><i className="uil uil-arrow-right text-emerald-600 align-middle ltr:mr-2 rtl:ml-2"></i>Create your own skin to match your brand</li>
-                            </ul>
 
-                            <h5 className="text-xl font-medium mb-4 mt-8">Information Provided Voluntarily :</h5>
-                            <p className="text-slate-400">In the 1960s, the text suddenly became known beyond the professional circle of typesetters and layout designers when it was used for Letraset sheets (adhesive letters on transparent film, popular until the 1980s) Versions of the text were subsequently included in DTP programmes such as PageMaker etc.</p>
+        <div className="absolute text-center z-10 bottom-5 right-0 left-0 mx-3">
+          <ul className="breadcrumb tracking-[0.5px] breadcrumb-light mb-0 inline-block">
+            <li className="inline breadcrumb-item text-[15px] font-semibold duration-500 ease-in-out text-white/50 hover:text-white">
+              <a href="index.html">Jobstack</a>
+            </li>
+            <li
+              className="inline breadcrumb-item text-[15px] font-semibold duration-500 ease-in-out text-white"
+              aria-current="page"
+            >
+              Privacy Policy
+            </li>
+          </ul>
+        </div>
+      </section>
+      <div className="relative">
+        <div className="shape overflow-hidden z-1 text-white dark:text-slate-900">
+          <svg
+            viewBox="0 0 2880 48"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </div>
+      </div>
 
-                            <div className="mt-8">
-                                <a href="#" className="btn bg-emerald-600 hover:bg-emerald-700 text-white rounded-md">Print</a>
-                            </div>
-                        </div>
-                    </div>
+      <section className="relative lg:py-24 py-16">
+        <div className="container">
+          <div className="md:flex justify-center">
+            <div className="md:w-3/4">
+              <div className="p-6 bg-white dark:bg-slate-900 shadow dark:shadow-gray-700 rounded-md">
+                <h5 className="text-xl font-medium mb-4">Overview :</h5>
+                <p className="text-slate-400">
+                  It seems that only fragments of the original text remain in
+                  the Lorem Ipsum texts used today. One may speculate that over
+                  the course of time certain letters were added or deleted at
+                  various positions within the text.
+                </p>
+                <p className="text-slate-400">
+                  In the 1960s, the text suddenly became known beyond the
+                  professional circle of typesetters and layout designers when
+                  it was used for Letraset sheets (adhesive letters on
+                  transparent film, popular until the 1980s) Versions of the
+                  text were subsequently included in DTP programmes such as
+                  PageMaker etc.
+                </p>
+                <p className="text-slate-400">
+                  There is now an abundance of readable dummy texts. These are
+                  usually used when a text is required purely to fill a space.
+                  These alternatives to the classic Lorem Ipsum texts are often
+                  amusing and tell short, funny or nonsensical stories.
+                </p>
+
+                <h5 className="text-xl font-medium mb-4 mt-8">
+                  We use your information to :
+                </h5>
+                <ul className="list-unstyled text-slate-400 mt-4">
+                  <li className="flex mt-2">
+                    <i className="uil uil-arrow-right text-emerald-600 align-middle ltr:mr-2 rtl:ml-2"></i>
+                    Digital Marketing Solutions for Tomorrow
+                  </li>
+                  <li className="flex mt-2">
+                    <i className="uil uil-arrow-right text-emerald-600 align-middle ltr:mr-2 rtl:ml-2"></i>
+                    Our Talented & Experienced Marketing Agency
+                  </li>
+                  <li className="flex mt-2">
+                    <i className="uil uil-arrow-right text-emerald-600 align-middle ltr:mr-2 rtl:ml-2"></i>
+                    Create your own skin to match your brand
+                  </li>
+                  <li className="flex mt-2">
+                    <i className="uil uil-arrow-right text-emerald-600 align-middle ltr:mr-2 rtl:ml-2"></i>
+                    Digital Marketing Solutions for Tomorrow
+                  </li>
+                  <li className="flex mt-2">
+                    <i className="uil uil-arrow-right text-emerald-600 align-middle ltr:mr-2 rtl:ml-2"></i>
+                    Our Talented & Experienced Marketing Agency
+                  </li>
+                  <li className="flex mt-2">
+                    <i className="uil uil-arrow-right text-emerald-600 align-middle ltr:mr-2 rtl:ml-2"></i>
+                    Create your own skin to match your brand
+                  </li>
+                </ul>
+
+                <h5 className="text-xl font-medium mb-4 mt-8">
+                  Information Provided Voluntarily :
+                </h5>
+                <p className="text-slate-400">
+                  In the 1960s, the text suddenly became known beyond the
+                  professional circle of typesetters and layout designers when
+                  it was used for Letraset sheets (adhesive letters on
+                  transparent film, popular until the 1980s) Versions of the
+                  text were subsequently included in DTP programmes such as
+                  PageMaker etc.
+                </p>
+
+                <div className="mt-8">
+                  <a
+                    href="#"
+                    className="btn bg-emerald-600 hover:bg-emerald-700 text-white rounded-md"
+                  >
+                    Print
+                  </a>
                 </div>
+              </div>
             </div>
-        </section>
-        {/* <footer className="relative bg-slate-900 dark:bg-slate-800">
+          </div>
+        </div>
+      </section>
+      {/* <footer className="relative bg-slate-900 dark:bg-slate-800">
             <div className="container">
                 <div className="grid grid-cols-1">
                     <div className="relative py-12">
@@ -294,7 +361,7 @@ const PrivacyPolicy = () => {
         
         <a href="#" onClick="topFunction()" id="back-to-top" class="back-to-top fixed hidden text-lg rounded-full z-10 bottom-5 ltr:right-5 rtl:left-5 h-9 w-9 text-center bg-emerald-600 text-white justify-center items-center"><i className="uil uil-arrow-up"></i></a> */}
     </div>
-  )
-}
+  );
+};
 
-export default PrivacyPolicy
+export default PrivacyPolicy;
