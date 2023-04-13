99af1211977936e3338ba839cf050bc089448e9a
Mehmood khan <mehmood.khan@ceative.co.uk>
2023-04-13 12:46:32 +0500

    Updated Job Details APi Integration

--- src/components/Pages/Jobs/JobDetails/components/JobInformation/JobInformation.jsx
+++ src/components/Pages/Jobs/JobDetails/components/JobInformation/JobInformation.jsx
@@ -1,5 +1,7 @@
 import React from "react";
- const JobInformation =()=>  {
+import moment from "moment";
+
+const JobInformation = ({ experience, jobType, location, created_at }) => {
   const jobInformationData = [
     {
       icon: "user-check",
@@ -9,17 +11,17 @@ import React from "react";
     {
       icon: "map-pin",
       title: "Location:",
-      value: "Beijing, China",
+      value: location,
     },
     {
       icon: "monitor",
       title: "Job Type:",
-      value: "Back-end Developer",
+      value: jobType,
     },
     {
       icon: "briefcase",
       title: "Experience:",
-      value: "2+ years",
+      value: experience,
     },
     {
       icon: "book",
@@ -34,35 +36,35 @@ import React from "react";
     {
       icon: "clock",
       title: "Date posted:",
-      value: "28th Feb, 2023",
+      value: moment(created_at).format("Do MMM, YYYY"),
     },
   ];
 
   return (
-   <>
-    <div className="lg:col-span-4 md:col-span-6">
-      <div className="shadow dark:shadow-gray-700 rounded-md bg-white dark:bg-slate-900 sticky top-20">
-        <div className="p-6">
-          <h5 className="text-lg font-semibold">Job Information</h5>
-        </div>
-        <div className="p-6 border-t border-slate-100 dark:border-t-gray-700">
-          <ul className="list-none">
-            {jobInformationData.map((info) => (
-              <li className="flex items-center mt-3" key={info.title}>
-                <i data-feather={info.icon} className="h-5 w-5"></i>
-                <div className="ltr:ml-4 rtl:mr-4">
-                  <p className="font-medium">{info.title}</p>
-                  <span className="text-emerald-600 font-medium text-sm">
-                    {info.value}
-                  </span>
-                </div>
-              </li>
-            ))}
-          </ul>
+    <>
+      <div className="lg:col-span-4 md:col-span-6">
+        <div className="shadow dark:shadow-gray-700 rounded-md bg-white dark:bg-slate-900 sticky top-20">
+          <div className="p-6">
+            <h5 className="text-lg font-semibold">Job Information</h5>
+          </div>
+          <div className="p-6 border-t border-slate-100 dark:border-t-gray-700">
+            <ul className="list-none">
+              {jobInformationData.map((info) => (
+                <li className="flex items-center mt-3" key={info.title}>
+                  <i data-feather={info.icon} className="h-5 w-5"></i>
+                  <div className="ltr:ml-4 rtl:mr-4">
+                    <p className="font-medium">{info.title}</p>
+                    <span className="text-emerald-600 font-medium text-sm">
+                      {info.value}
+                    </span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
-}
-export default JobInformation
+};
+export default JobInformation;
