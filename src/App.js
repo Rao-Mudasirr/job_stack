1b4086a080bf104cb2c12b4f9c99047f2bae9d94
Muhammad Waqas <mr.vaqas4@gmail.com>
2023-04-13 12:37:26 +0500

    #comment remove auth page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
-import Login from './components/Pages/AuthPages/Login';
 import {
   Routes,
   Route,
 } from "react-router-dom";
-import Signup from './components/Pages/AuthPages/Signup';
 import Home from './components/Pages/HomePages/Home';
 
 function App() {
   return (
     <Routes>
-      <Route exact path={"/login"} element={<Login />} />
-      <Route exact path='/signup' element={<Signup />} />
+      <Route exact path={"/"} element={<Home />} />
     </Routes>
   );
 }
--- src/components/Pages/HomePages/Home.jsx
+++ src/components/Pages/HomePages/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Hero from './Components/Hero'
-import PopularCategories from './Components/PopularCategories'
+// import PopularCategories from './Components/PopularCategories'
 import PopularJobs from './Components/PopularJobs'
-import BestCompanies from './Components/BestCompanies'
+// import BestCompanies from './Components/BestCompanies'
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <PopularCategories/>
+      {/* <PopularCategories/> */}
       <PopularJobs/>
-      <BestCompanies/>
+      {/* <BestCompanies/> */}
     </>
   )
 }
