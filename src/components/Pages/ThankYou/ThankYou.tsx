7664505990de3a0aac8f9d032bf51f9997ab2738
shahbaz mughal <shahbaz.mughal@ceative.co.uk>
2023-04-19 16:18:49 +0500

    #comments

--- src/components/Pages/ThankYou/ThankYou.tsx
+++ src/components/Pages/ThankYou/ThankYou.tsx
@@ -32,51 +32,6 @@ const ThankYou = () => {
           </div>
         </div>
       </section>
-      <div className="fixed bottom-3 right-3">
-        <a
-          href="#"
-          className="back-button btn btn-icon bg-emerald-600 hover:bg-emerald-700 border-emerald-600 hover:border-emerald-700 text-white rounded-full"
-        >
-          <i data-feather="arrow-left" className="h-4 w-4"></i>
-        </a>
-      </div>
-      <div className="fixed top-1/4 -left-2 z-50 hidden sm:block">
-        <span className="relative inline-block rotate-90">
-          <input
-            type="checkbox"
-            className="checkbox opacity-0 absolute"
-            id="chk"
-          />
-          <label
-            className="label bg-slate-900 dark:bg-white shadow dark:shadow-gray-800 cursor-pointer rounded-full flex justify-between items-center p-1 w-14 h-8"
-            htmlFor="chk"
-          >
-            <i className="uil uil-moon text-[20px] text-yellow-500"></i>
-            <i className="uil uil-sun text-[20px] text-yellow-500"></i>
-            <span className="ball bg-white dark:bg-slate-900 rounded-full absolute top-[2px] left-[2px] w-7 h-7"></span>
-          </label>
-        </span>
-      </div>
-
-      <div className="fixed top-1/2 -right-11 z-50 hidden sm:block">
-        <a
-          href="https://1.envato.market/jobstack"
-          target="_blank"
-          className="py-1 px-3 relative inline-block rounded-b-md -rotate-90 bg-white dark:bg-slate-900 shadow-md dark:shadow dark:shadow-gray-700 font-semibold"
-        >
-          <i className="mdi mdi-cart-outline mr-1"></i> Download
-        </a>
-      </div>
-      <div className="fixed top-[40%] -left-3 z-50">
-        <a href="#" id="switchRtl">
-          <span className="py-1 px-3 relative inline-block rounded-t-md -rotate-90 bg-white dark:bg-slate-900 shadow-md dark:shadow dark:shadow-gray-800 font-semibold rtl:block ltr:hidden">
-            LTR
-          </span>
-          <span className="py-1 px-3 relative inline-block rounded-t-md -rotate-90 bg-white dark:bg-slate-900 shadow-md dark:shadow dark:shadow-gray-800 font-semibold ltr:block rtl:hidden">
-            RTL
-          </span>
-        </a>
-      </div>
     </div>
   );
 };
