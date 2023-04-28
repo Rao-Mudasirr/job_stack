5727632578f640578a7f279a06b76b4f0636753a
MuhammadWaqas525 <MuhammadWaqas@github.com>
2023-04-28 16:09:54 +0500

    remove searchbar

--- src/components/Pages/JobList/JobList.jsx
+++ src/components/Pages/JobList/JobList.jsx
@@ -41,7 +41,7 @@ export const JobList = () => {
                 </div>
             </div>
             <section className="relative -mt-[42px] md:pb-24 pb-16">
-                <div className="container z-1">
+                {/* <div className="container z-1">
                     <div className="d-flex" id="reserve-form">
                         <div className="md:w-5/6 mx-auto">
                             <div className="lg:col-span-10">
@@ -81,8 +81,8 @@ export const JobList = () => {
                             </div>
                         </div>
                     </div>
-                </div>
-                <div>
+                </div> */}
+                <div className='mt-8'>
                     <div className="overflow-x-auto">
                         <div className="min-w-screen min-h-screen flex items-start justify-center font-sans" style={{ marginInline: "15%" }}>
                             <div className="w-full lg:w-5/6" >
