46ef65a2afb14e2319e918e99394f40e20c89776
Mehmood khan <mehmood.khan@ceative.co.uk>
2023-05-12 21:55:05 +0500

    Last Update of main Quiz

--- src/components/Pages/MainQuiz/mainQuiz.jsx
+++ src/components/Pages/MainQuiz/mainQuiz.jsx
@@ -22,14 +22,6 @@ export const MainQuiz = () => {
     isToggle: false,
     type: "",
   });
-
-  // const handleVisibilityChange = () => {
-  //   if (document.hidden) {
-  //     console.log("end Test");
-  //   }
-  // };
-  // document.addEventListener("visibilitychange", handleVisibilityChange);
-
   const handleChange = (e, questionsId) => {
     setOption(e.target.value);
     setQuestionId(questionsId);
@@ -40,10 +32,8 @@ export const MainQuiz = () => {
   };
   const isToken = localStorage.getItem("token");
   const isLastQuestion = index === jobTest.length - 1;
-
   const nextQuestion = async () => {
-    // Will move to next index only if option state is not empty
-    if (option !== "") {
+    if (selectedAnswers[jobTest[index]?.id]) {
       try {
         const response = await axios.post(
           "https://jobs.orcaloholding.co.uk/api/test/submit-answer",
@@ -60,45 +50,30 @@ export const MainQuiz = () => {
           }
         );
         const { data } = response;
-        if (index < jobTest.length - 1) {
-          localStorage.setItem("questionIndex", JSON.stringify(index + 1));
-          localStorage.setItem("disabledIndex", JSON.stringify(disabled + 1));
+        if (data?.status) {
+          setSnackbar({ title: data?.msg, isToggle: true, type: "success" });
+          if (!isLastQuestion) {
+            setIndex(index + 1);
+            setDisabled(disabled + 1);
+          } else {
+            setIndex(index);
+            setDisabled(disabled + 1);
+          }
         } else {
-          localStorage.setItem("questionIndex", JSON.stringify(index));
-          localStorage.setItem("disabledIndex", JSON.stringify(disabled + 1));
+          setSnackbar({ title: data?.msg, isToggle: true, type: "error" });
         }
-        setSnackbar({
-          title: data?.msg,
-          isToggle: true,
-          type: "success",
-        });
-        setDisabled(disabled + 1);
       } catch (error) {
         console.log(error);
-        setSnackbar({
-          title: error?.message,
-          isToggle: true,
-          type: "error",
-        });
+        setSnackbar({ title: error?.message, isToggle: true, type: "error" });
       }
-
-      // Move to the next question if not at the last question
-      if (!isLastQuestion) {
-        setIndex(index + 1);
-      }
-      setDisabled(disabled + 1);
-      setOption("");
-      setQuestionId("");
-    }
-    option === "" &&
+    } else {
       setSnackbar({
         title: "Please Select One Answer",
         isToggle: true,
         type: "error",
       });
-    // setOption("");
+    }
   };
-
   const handleTimeUp = () => {
     setTimeUp(true);
     localStorage.removeItem("timer");
@@ -109,24 +84,19 @@ export const MainQuiz = () => {
     }, 2000);
   };
   const prevQuestion = () => {
-    index !== 0 && setIndex(index - 1);
-
-    setDisabled(disabled - 1);
-
-    setIndex(index - 1);
-
-    localStorage.setItem("questionIndex", JSON.stringify(index - 1));
-
-    localStorage.setItem("disabledIndex", JSON.stringify(disabled - 1));
+    if (disabled > jobTest?.length) {
+      setIndex(index - 1);
+      setDisabled(disabled - 2);
+    } else {
+      setIndex(index - 1);
+      setDisabled(disabled - 1);
+    }
   };
-
   const endTestHandler = async () => {
     try {
       const response = await axios.post(
         "https://jobs.orcaloholding.co.uk/api/test/end",
-        {
-          attempt_id: location?.state?.attemptData?.attempt?.id,
-        },
+        { attempt_id: location?.state?.attemptData?.attempt?.id },
         {
           headers: {
             Authorization: `Bearer ${isToken}`,
@@ -134,82 +104,71 @@ export const MainQuiz = () => {
           },
         }
       );
-      navigate(
-        "/quiz-card",
-        {
-          state: response?.data,
-        },
-        (replace = true)
-      );
+      navigate("/quiz-card", { state: response?.data }, (replace = true));
     } catch (error) {
       console.log(error);
     }
   };
-
   useEffect(() => {
-    const getIndex = localStorage.getItem("questionIndex");
-    const getDisabledIndex = localStorage.getItem("disabledIndex");
     const timers = localStorage.getItem("timer");
-
     setTimeLeft(timers);
-    setIndex(Number(getIndex));
-    setDisabled(Number(getDisabledIndex));
-    
     const handleBeforeUnload = (event) => {
       event.preventDefault();
-      event.returnValue = ''; // Required for Chrome and Firefox
+      event.returnValue = "";
     };
-
     const enableConfirmationMessage = () => {
-      window.addEventListener('beforeunload', handleBeforeUnload);
+      window.addEventListener("beforeunload", handleBeforeUnload);
     };
-
     const disableConfirmationMessage = () => {
-      window.removeEventListener('beforeunload', handleBeforeUnload);
+      window.removeEventListener("beforeunload", handleBeforeUnload);
     };
-
     enableConfirmationMessage();
-
     return () => {
       disableConfirmationMessage();
     };
-    // const handleVisibilityChange = () => {
-    //   if (document.hidden) {
-    //     /* end the test */
-    //     endTestHandler()
-    //   }
-    // };
-    // document.addEventListener("visibilitychange", handleVisibilityChange);
   }, []);
-
+  console.log(disabled, "disabled");
+  console.log(index, "index");
+  console.log(jobTest?.length, "length");
   return (
     <div dir="ltr">
+      {" "}
       <section className="bg-slate-50 dark:bg-slate-800 md:py-24 py-16">
-        <GlobalSnackBar isOpenSnack={snackbar} setIsOpenSnack={setSnackbar} />
+        {" "}
+        <GlobalSnackBar
+          isOpenSnack={snackbar}
+          setIsOpenSnack={setSnackbar}
+        />{" "}
         <div className="container mt-10 mx-auto rounded-lg border border-gray-100 p-4 sm:p-6 lg:p-8 bg-emerald-600/5 border-emerald-600/10">
+          {" "}
           {timeUp ? (
             <div className="text-center">Time's up!</div>
           ) : (
             <>
-              <span className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-emerald-600"></span>
+              {" "}
+              <span className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-emerald-600"></span>{" "}
               <div className="flex text-2xl font-bold justify-between align-center mb-10">
-                <h1>Total Question : {` ${jobTest?.length}`}</h1>
+                {" "}
+                <h1>Total Question : {` ${jobTest?.length}`}</h1>{" "}
                 <QuizTimer
                   timeLeft={timeLeft}
                   setTimeLeft={setTimeLeft}
                   onTimeUp={handleTimeUp}
-                />
-              </div>
+                />{" "}
+              </div>{" "}
               <div className="flex text-2xl font-bold mb-10">
-                Q.{`${index + 1}`}
+                {" "}
+                Q.{`${index + 1}`}{" "}
                 <h1
                   className="text-xl ml-3"
                   dangerouslySetInnerHTML={{ __html: jobTest[index]?.question }}
-                />
-              </div>
+                />{" "}
+              </div>{" "}
               <form>
+                {" "}
                 {jobTest[index]?.options?.map((opt) => (
                   <div key={opt?.id} className="mt-2 flex items-center">
+                    {" "}
                     <input
                       checked={selectedAnswers[jobTest[index]?.id] === opt?.id}
                       name="radio-group"
@@ -219,20 +178,19 @@ export const MainQuiz = () => {
                       type="radio"
                       onChange={(e) => handleChange(e, jobTest[index]?.id)}
                       className=" h-5 w-5  accent-emerald-800 "
-                      disabled={disabled >= jobTest?.length}
-                    />
-                    <label
-                      htmlFor={opt?.option}
-                      className="ml-2   text-gray-700"
-                    >
-                      {opt?.option}
-                    </label>
+                      disabled={disabled > jobTest?.length}
+                    />{" "}
+                    <label htmlFor={opt?.option} className="ml-2 text-gray-700">
+                      {" "}
+                      {opt?.option}{" "}
+                    </label>{" "}
                   </div>
-                ))}
-              </form>
+                ))}{" "}
+              </form>{" "}
             </>
-          )}
+          )}{" "}
           <div className="mt-20 w-100 flex justify-between">
+            {" "}
             <button
               disabled={index === 0}
               onClick={prevQuestion}
@@ -242,36 +200,38 @@ export const MainQuiz = () => {
                   : "bg-emerald-600/5 hover:bg-emerald-600 border-emerald-600 hover:border-emerald-600 text-emerald-600 hover:text-white"
               } rounded-full`}
             >
+              {" "}
               Previous{" "}
-            </button>
-            {disabled >= jobTest?.length && (
+            </button>{" "}
+            {disabled > jobTest?.length && (
+              <button
+                className="w-32 btn bg-emerald-600/5 hover:bg-emerald-600 border-emerald-600 hover:border-emerald-600 text-emerald-600 hover:text-white rounded-full"
+                onClick={() => {
+                  endTestHandler();
+                  localStorage.removeItem("timer");
+                  localStorage.removeItem("questionIndex");
+                  localStorage.removeItem("disabledIndex");
+                }}
+              >
+                {" "}
+                End Test{" "}
+              </button>
+            )}{" "}
             <button
-              className="w-32 btn bg-emerald-600/5 hover:bg-emerald-600 border-emerald-600 hover:border-emerald-600 text-emerald-600 hover:text-white rounded-full"
-              onClick={() => {
-                endTestHandler();
-                localStorage.removeItem("timer");
-                localStorage.removeItem("questionIndex");
-                localStorage.removeItem("disabledIndex");
-              }}
-            >
-              End Test
-            </button>
-          )}
-            <button
-              disabled={disabled >= jobTest?.length}
+              disabled={disabled > jobTest?.length}
               onClick={nextQuestion}
               className={` ml-2 btn w-32 ${
-                disabled >= jobTest?.length
+                disabled > jobTest?.length
                   ? "bg-gray-600/5 border-gray-600 hover:border-gray-600 text-gray-600 hover:text-gray"
                   : "bg-emerald-600/5 hover:bg-emerald-600 border-emerald-600 hover:border-emerald-600 text-emerald-600 hover:text-white"
               } rounded-full`}
             >
-              Next
-            </button>
-          
-          </div>
-        </div>
-      </section>
+              {" "}
+              Next{" "}
+            </button>{" "}
+          </div>{" "}
+        </div>{" "}
+      </section>{" "}
     </div>
   );
 };
